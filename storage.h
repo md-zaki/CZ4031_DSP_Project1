95fac7a364ff6c10954cff6aec154f52d030fa19
Md Zaki <mdzaki333@gmail.com>
2023-01-31 17:09:23 +0800

    create storage class in header file

---
+++ storage.h
@@ -0,0 +1,44 @@
+#ifndef STORAGE_H
+#define STORAGE_H
+
+#include <iostream>
+using namespace std;
+
+/* Structure representing one record
+following the column names and datatypes found
+data.tsv
+*/
+struct Record {
+    char tconst[10]; //movie id
+    float averageRating; //average rating of movie
+    unsigned int numVotes; //number of ratings on movie
+};
+
+class Storage {
+    private:
+        unsigned int totalDiskSize; // size in bytes of the whole disk/main memory
+        unsigned int blockSize; //size in bytes of 1 block
+        unsigned int usedBlocksSize; //size in bytes of all used blocks
+        unsigned int assignedRecordsSize; // size in bytes of all records assigned to blocks
+
+        int totalBlocks; // total number of blocks in database system
+        int numUsedBlocks; //total number of blocks used
+        int numRecordsAssigned; // number of records assigned to database system
+
+        unsigned int *diskPtr; // pointer to start of storage
+        unsigned int *currentBlockPtr; // pointer to start of current block
+
+
+    public:
+        /**
+         * Constructor for Storage class
+         * @param totalDiskSize size in bytes of the whole disk/main memory
+         * @param blockSize size in bytes of 1 block
+        */
+        Storage(unsigned int totalDiskSize,unsigned int blockSize);
+
+        //Destructor
+        ~Storage(); 
+};
+
+#endif
